39601e3fc4fec0fe38d59218aac27c4eefcb366f
yusai <yusai@hhdatabase.com.cn>
2020-12-01 18:43:33 +0800

    personal note code first commit!

---
+++ src/test/java/com/asteroid/pncode/PnCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asteroid.pncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
