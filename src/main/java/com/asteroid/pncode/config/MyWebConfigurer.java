23c898468604030bc39de893fa6cd884754a3f44
yusai <yusai@hhdatabase.com.cn>
2020-12-07 16:04:46 +0800

    修改提交

--- src/main/java/com/asteroid/pncode/config/MyWebConfigurer.java
+++ src/main/java/com/asteroid/pncode/config/MyWebConfigurer.java
@@ -15,19 +15,15 @@ public class MyWebConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(getLoginIntercepter())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/index.html")
-                .excludePathPatterns("/api/login")
-                .excludePathPatterns("/api/logout");
+        registry.addInterceptor(getLoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/index.html");
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        //所有请求都允许跨域
         registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 
--- src/main/java/com/asteroid/pncode/controller/LoginController.java
+++ src/main/java/com/asteroid/pncode/controller/LoginController.java
@@ -5,15 +5,13 @@ import com.asteroid.pncode.result.Result;
 import com.asteroid.pncode.result.ResultFactory;
 import com.asteroid.pncode.service.UserService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.crypto.SecureRandomNumberGenerator;
-import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author YuSai
@@ -27,18 +25,16 @@ public class LoginController {
     @CrossOrigin
     @PostMapping(value = "/api/login")
     @ResponseBody
-    public Result login(@RequestBody User requestUser) {
+    public Result login(@RequestBody User requestUser, HttpSession session) {
         String username = requestUser.getUsername();
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, requestUser.getPassword());
-        usernamePasswordToken.setRememberMe(true);
-        try {
-            subject.login(usernamePasswordToken);
-
-            return ResultFactory.buildSuccessResult(username);
-        } catch (AuthenticationException e) {
+        username = HtmlUtils.htmlEscape(username);
+        User user = userService.get(username, requestUser.getPassword());
+        if (null == user) {
             String message = "账号密码错误";
             return ResultFactory.buildFailResult(message);
+        } else {
+            session.setAttribute("user", user);
+            return ResultFactory.buildSuccessResult(username);
         }
     }
 
